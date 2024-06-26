954b0f3e291cecd2588c1d77c191b123463e46c8
Tom Medvitz <tom@techelevator.com>
2024-06-26 12:35:22 -0400

    After class

---
+++ module-2/13_Server_Side_APIs_Part_1/lecture-final-tom/server/src/main/java/com/techelevator/reservations/controllers/ReservationController.java
@@ -0,0 +1,30 @@
+package com.techelevator.reservations.controllers;
+
+import com.techelevator.reservations.dao.HotelDao;
+import com.techelevator.reservations.dao.MemoryHotelDao;
+import com.techelevator.reservations.dao.MemoryReservationDao;
+import com.techelevator.reservations.dao.ReservationDao;
+import com.techelevator.reservations.model.Reservation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ReservationController {
+
+    private ReservationDao rezDao;
+
+    public ReservationController() {
+        HotelDao hDao = new MemoryHotelDao();
+        this.rezDao = new MemoryReservationDao(hDao);
+    }
+
+
+    // Create a new reservation!
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/reservations", method = RequestMethod.POST)
+    public Reservation addReservation(@RequestBody Reservation newReservation) {
+        return rezDao.createReservation(newReservation);
+    }
+
+
+}
