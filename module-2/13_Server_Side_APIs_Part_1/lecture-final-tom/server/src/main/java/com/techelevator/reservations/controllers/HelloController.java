c3c7e795821a1608e7f05354c718e5fe538e25b9
Tom Medvitz <tom@techelevator.com>
2024-06-27 09:06:55 -0400

    Server Side lecture-final-tom

--- module-2/13_Server_Side_APIs_Part_1/lecture-final-tom/server/src/main/java/com/techelevator/reservations/controllers/HelloController.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture-final-tom/server/src/main/java/com/techelevator/reservations/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package com.techelevator.reservations.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,7 @@ public class HelloController {
 
 
     //  HTTP GET: /hello
+    @ResponseStatus(HttpStatus.OK) // 200 OK
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello World!";
--- module-2/13_Server_Side_APIs_Part_1/lecture-final-tom/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture-final-tom/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.techelevator.reservations.dao.MemoryHotelDao;
 import com.techelevator.reservations.dao.MemoryReservationDao;
 import com.techelevator.reservations.dao.ReservationDao;
 import com.techelevator.reservations.model.Hotel;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,17 +29,20 @@ public class HotelController {
      */
 
     // GET /hotels
-    // GET /hotels?city=Cleveland&state=Ohio
+    // GET /hotels?city=x&state=y
+    @ResponseStatus(HttpStatus.OK) // 200 OK
     @RequestMapping(path = "/hotels", method = RequestMethod.GET)
     public List<Hotel> list(@RequestParam(defaultValue = "") String city, @RequestParam(defaultValue = "") String state) {
         return hotelDao.getHotelsByStateAndCity(state, city);
     }
-        /**
-         * Return hotel information
-         *
-         * @param id the id of the hotel
-         * @return all info for a given hotel
-         */
+
+    /**
+     * Return hotel information
+     *
+     * @param id the id of the hotel
+     * @return all info for a given hotel
+     */
+    @ResponseStatus(HttpStatus.OK) // 200 OK
     @RequestMapping(path = "/hotels/{hotelId}", method = RequestMethod.GET)
     public Hotel get(@PathVariable(name = "hotelId") int id) {
         return hotelDao.getHotelById(id);
