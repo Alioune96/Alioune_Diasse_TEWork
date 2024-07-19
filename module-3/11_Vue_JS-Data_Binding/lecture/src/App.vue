3bf0b6ae5e262f7c60f5f353fd504ed861c73257
Tom Medvitz <tom@techelevator.com>
2024-07-19 12:25:13 -0400

    Vue Week Tutorials (and first exercise)

---
+++ module-3/11_Vue_JS-Data_Binding/lecture/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- HTML Section!  notice these comments are allllll different -->
+  <div class="main">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <!-- Step 3 (of using sub-components): Use the component as a custom HTML tag -->
+    <ProductReview />
+
+
+     
+  </div>
+</template>
+
+
+<script>
+// JavaScript section!
+import ProductReview from './components/ProductReview.vue'; // Step 1: Import it!
+
+export default {
+  components: {
+    ProductReview   // Step 2: Register it!
+  }
+}
+</script>
+
+
+<style>
+/* CSS Section! */
+.main {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
